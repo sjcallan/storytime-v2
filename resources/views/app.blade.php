a7ea255ce07ac24400a64a53a0fc5b7401ba9bda
Steve Callan <sjcallan@gmail.com>
2025-12-29 07:28:01 -0500

    Install Google Analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -51,6 +51,15 @@
         @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1MXP6S9ZJE"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-1MXP6S9ZJE');
+</script>
     <body class="font-sans antialiased">
         @inertia
     </body>
